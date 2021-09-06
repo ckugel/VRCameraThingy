89c3d1e252bd3971e32a7f2499899730ebc85687
Caleb Kugel <calebkugel1@gmail.com>
2021-09-05 21:46:49 -0500

    weather in a thread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import threading
+import time
 
 import cv2
 
@@ -21,8 +22,8 @@ def updateVals():
     width = b
 
 def app():
-    #thread1 = threading.Thread(target=updateVals)
-    #thread1.start()
+    thread1 = threading.Thread(name='wheather', target=scraper)
+    thread1.start()
     cap = cv2.VideoCapture(1)
 
     # Check if the webcam is opened correctly
@@ -44,9 +45,9 @@ def app():
             y = 50
             yOffset = 30
             stringer = scraper()
-            frame = cv2.putText(frame, str(scraper()[0]), (50, y), cv2.FONT_HERSHEY_SIMPLEX,
+            frame = cv2.putText(frame, str(getScraped()[0]), (50, y), cv2.FONT_HERSHEY_SIMPLEX,
                                 0.75, (255, 0, 255), 2, cv2.LINE_AA)
-            frame = cv2.putText(frame, str(scraper()[1]), (50, y + yOffset), cv2.FONT_HERSHEY_SIMPLEX,
+            frame = cv2.putText(frame, str(getScraped()[1]), (50, y + yOffset), cv2.FONT_HERSHEY_SIMPLEX,
                                 0.75, (255, 0, 255), 2, cv2.LINE_AA)
 
         cv2.imshow('Camera', frame)
@@ -59,32 +60,39 @@ def app():
     cv2.destroyAllWindows()
 
 def scraper():
-    # importing requests and json
-    import requests
-    # base URL
-    BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-    CITY = "Minneapolis"
-    API_KEY = "a4607507018ae05d3610c90156b45ecd"
-    # upadting the URL
-    URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
-    # HTTP request
-    response = requests.get(URL)
-    # checking the status code of the request
-    if response.status_code == 200:
-        # getting data in the json format
-        data = response.json()
-        # getting the main dict block
-        main = data['main']
-        # getting temperature
-        temperature = main['temp']
-        # weather report
-        report = data['weather']
+    while True:
+        global temperature, report
+        # importing requests and json
+        import requests
+        # base URL
+        BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+        CITY = "Minneapolis"
+        API_KEY = "a4607507018ae05d3610c90156b45ecd"
+        # upadting the URL
+        URL = BASE_URL + "q=" + CITY + "&appid=" + API_KEY
+        # HTTP request
+        response = requests.get(URL)
+        # checking the status code of the request
+        if response.status_code == 200:
+            # getting data in the json format
+            data = response.json()
+            # getting the main dict block
+            main = data['main']
+            # getting temperature
+            temperature = main['temp']
+            # weather report
+            report = data['weather']
 
-        return 'Temperature: ' + str(int((int(temperature) - 273.15) / 5 * 9 + 32)), 'Weather Report: ' + report[0]['description']
-    else:
-        # showing the error message
-        print("Error in the HTTP request")
+        else:
+            # showing the error message
+            print("Error in the HTTP request")
 
+        time.sleep(59)
+
+def getScraped():
+    global temperature, report
+    return 'Temperature: ' + str(int((int(temperature) - 273.15) / 5 * 9 + 32)), 'Weather Report: ' + report[0][
+        'description']
 
 # TODO: remove this
 if __name__ == '__main__':
